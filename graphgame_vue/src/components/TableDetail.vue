f0c03cfa0a03020ab4f87cde2d497c971c9f75ab
tobe <tobeg3oogle@gmail.com>
2022-11-01 17:55:01 +0800

    Add table tools

--- graphgame_vue/src/components/TableDetail.vue
+++ graphgame_vue/src/components/TableDetail.vue
@@ -2,6 +2,25 @@
 <template>
   <h1>TableDetail</h1>
 
+  <vxe-grid ref="nodesGridTable" v-bind="nodesTableOptions" v-on="nodesTableEvents">
+    <template #id_edit="{ row }">
+      <vxe-input v-model="row.id"></vxe-input>
+    </template>
+    <template #name_edit="{ row }">
+      <vxe-input v-model="row.name"></vxe-input>
+    </template>
+    <template #display_name_edit="{ row }">
+      <vxe-input v-model="row.display_name"></vxe-input>
+    </template>
+    <template #note_edit="{ row }">
+      <vxe-input v-model="row.note"></vxe-input>
+    </template>
+    <template #weight_edit="{ row }">
+      <vxe-input v-model="row.weight"></vxe-input>
+    </template>
+  </vxe-grid>
+
+
   <h2>Nodes:</h2>
   <vxe-table border :data="nodes">
     <vxe-column field="id" title="Id"></vxe-column>
@@ -27,24 +46,118 @@
   </vxe-table>
 </template>
 
-<script>
-import { ref, onMounted} from 'vue'
-
+<script lang="ts">
 import axios from 'axios'
+import { defineComponent, reactive, ref, onMounted} from 'vue'
+import { VXETable, VxeGridInstance, VxeGridListeners, VxeGridProps } from 'vxe-table'
 
-export default {
+export default defineComponent({
   name: "TableDetail",
+  props: {
+    page_name: String,
+  },
   setup () {
     const dbName = ref("cyberpunk_edgerunner");
     let nodes = ref([]);
     let edges = ref([]);
     let groups = ref([]);
 
+
+
+    const nodesGridTable = ref<VxeGridInstance>()
+    const nodesTableOptions = reactive<VxeGridProps>({
+      border: true,
+      keepSource: true,
+      id: 'toolbar_demo_1',
+      height: 530,
+      printConfig: {},
+      importConfig: {},
+      exportConfig: {},
+      columnConfig: {
+        resizable: true
+      },
+      customConfig: {
+        storage: true
+      },
+      editConfig: {
+        trigger: 'click',
+        mode: 'row',
+        showStatus: true
+      },
+      columns: [
+        { field: 'id', title: 'Id', slots: { edit: 'id_edit' } },
+        { field: 'name', title: 'Name', slots: { edit: 'name_edit' } },
+        { field: 'display_name', title: 'Display Name', slots: { edit: 'display_name_edit' } },
+        { field: 'note', title: 'Note', slots: { edit: 'note_edit' } },
+        { field: 'weight', title: 'Weight', slots: { edit: 'weight_edit' } }
+      ],
+      toolbarConfig: {
+        buttons: [
+          { code: 'myInsert', name: 'New', status: 'primary' },
+          { code: 'mySave', name: 'Save', status: 'success' },
+        ],
+        import: true,
+        export: true,
+        zoom: true,
+        custom: true
+      },
+      /*
+      data: [
+      {
+        "display_name": "David Martinez", 
+        "name": "david", 
+        "weight": 0.238291
+      }, 
+      {
+        "display_name": "Lucy", 
+        "name": "lucy", 
+        "weight": 0.123158
+      }]*/
+      data: []
+    })
+
+    const nodesTableEvents: VxeGridListeners = {
+      toolbarButtonClick ({ code }) {
+        const $grid = nodesGridTable.value
+        switch (code) {
+          case 'myInsert': {
+            $grid.insert({
+              
+            })
+            break
+          }
+          case 'mySave': {
+            const { insertRecords, removeRecords, updateRecords } = $grid.getRecordset()
+            VXETable.modal.message({ content: `新增 ${insertRecords.length} 条，删除 ${removeRecords.length} 条，更新 ${updateRecords.length} 条`, status: 'success' })
+            break
+          }
+          case 'myExport': {
+            $grid.exportData({
+              type: 'csv'
+            })
+            break
+          }
+        }
+      },
+      toolbarToolClick ({ code }) {
+        const $grid = nodesGridTable.value
+        switch (code) {
+          case 'myPrint': {
+            $grid.print()
+            break
+          }
+        }
+      }
+    }
+
+
     onMounted(() => {
 
       axios.get(`http://127.0.0.1:7788/api/${dbName.value}/nodes`)
         .then(response => {
           nodes.value = response.data.nodes;
+
+          nodesTableOptions.data = response.data.nodes;
         })
         .catch(error => {
           console.log(error);
@@ -67,13 +180,19 @@ export default {
         });
     })
 
+
+
+
     return {
       nodes,
       edges,
-      groups
+      groups,
+      nodesGridTable,
+      nodesTableOptions,
+      nodesTableEvents
     }
   }
-}
+})
 </script>
 
 <style scoped>
