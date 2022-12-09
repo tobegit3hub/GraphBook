1b9a286f16f1223c4955a86d2389cf5e39abc150
tobe <tobeg3oogle@gmail.com>
2022-12-09 20:31:58 +0800

    Add script to reset website service

---
+++ tools/website/reset_website.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -x
+set -e
+
+cd "$(dirname "$0")"
+
+# Pull latest code
+git pull origin main
+git submodule update --init
+
+# Update frontend
+cd ../../frontend/
+npm install
+npm run build
+
+# Cleanup old data
+cd ../tools/website/
+ps aux | grep './server.py' | grep -v "grep" | awk '{print $2}' | xargs kill -9
+mysql -uroot -proot < ./drop_database.sql
+
+# Import topics and start
+cd ../../python/server/
+./import_official_topics.py ./topicland_website.py
+nohup ./server.py ./topicland_website.py  &
